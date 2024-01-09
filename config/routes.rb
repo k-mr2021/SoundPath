1b674886f83727307d940216630b051bd0c9fd62
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2024-01-09 15:44:42 +0000

    [Add] admin/ログイン実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
