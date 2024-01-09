7650d91501d79d40df6e1dc9460f70e27b4b0ccd
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2024-01-09 15:25:52 +0000

    [Add] deviseインストール

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "e6648c728cf746b18a29dbb771f9297d.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
